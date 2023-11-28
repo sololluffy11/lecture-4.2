b928665985d6fb0449ed01fcc78af72d2d0fd51a
sololuffy1 <sololevelling12@gmail.com>
2023-11-28 15:28:02 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+// let arr = [1,2,"hello",44]
+// console.log(arr[2]);
+
+// let arr = [1,2,3,4]
+// arr.push(5,"hi")
+// console.log(arr);
+
+// let arr = [1,2,3,4]
+// arr.pop()
+// console.log(arr);
+
+// let arr = [1,2,3,4]
+// arr.unshift(6)
+// console.log(arr);
+
+// let arr = [1,2,3,4]
+// arr.shift(6)
+// console.log(arr);
+
+
+// function task(){
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let cnt = arr.length
+// for(let i=0; i<cnt; i++){
+//     if(arr[i]%2==0){
+//         console.log(arr[i]);
+//     }
+// }
+// }
+// task()
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let res = arr.slice(-5,-1)
+// console.log(res);
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let res = arr.slice(-5,-1)
+// console.log(res);
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let res = arr.splice(3)
+// console.log(res);
+ 
+
